c79bdb8932d31c3d001cd0a2d51be7bce91d1b2f
Osvaldo S. Fernandez V <osvaldof22@hotmail.com>
2020-06-05 17:20:44 -0400

    Using java stream for the privileges and roles. Some doc added.

--- src/main/java/com/project/ccts/service/security/SecurityService.java
+++ src/main/java/com/project/ccts/service/security/SecurityService.java
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Interface for user validation in the database.
@@ -35,19 +36,41 @@ public class SecurityService implements UserDetailsService {
         this.credentialRepository = credentialRepository;
     }
 
+    /**
+     * User details implementation function that validate the user when try to login
+     *
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Finding the user based on the username
         Credential credential = credentialRepository.findByUsername(username);
 
+        //Validating the user
         if (credential == null) {
             String message = String.format("Username %s not found!!", username);
             Logger.getInstance().getLog(getClass()).warn(message);
             throw new UsernameNotFoundException(message);
         }
 
-        return new org.springframework.security.core.userdetails.User(credential.getUsername(), credential.getPassword(), credential.getAuthenticated(), true, true, true, getAuthorities(credential.getRoles()));
+        //Returning a new UserDetails for Spring management with the authorities, username, password...
+        return new org.springframework.security.core.userdetails.User(credential.getUsername(),
+                credential.getPassword(),
+                credential.getAuthenticated(),
+                true,
+                true,
+                true,
+                getAuthorities(credential.getRoles()));
     }
 
+    /**
+     * Custom function to validate the username and roles that comes with the token JWT.
+     *
+     * @param username
+     * @return Collection of Granted Authorities
+     */
     public Collection<? extends GrantedAuthority> getAuthorities(String username) {
         Credential credential = credentialRepository.findByUsername(username);
 
@@ -58,28 +81,40 @@ public class SecurityService implements UserDetailsService {
         return getAuthorities(credential.getRoles());
     }
 
+    /**
+     * Function to extract all the authorities based on the roles.
+     *
+     * @param roles
+     * @return
+     */
     private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
         return getGrantedAuthorities(getPrivileges(roles));
     }
 
-    private List<String> getPrivileges(Collection<Role> roles) {
-        List<String> privileges = new ArrayList<>();
-        List<Privilege> collection = new ArrayList<>();
-        for (Role role : roles) {
-            collection.addAll(role.getPrivileges());
-        }
-        for (Privilege item : collection) {
-            privileges.add(item.getName());
-        }
-        return privileges;
+    /**
+     * Function to extract all the privileges of a collection of roles.
+     *
+     * @param roles
+     * @return
+     */
+    private Collection<String> getPrivileges(Collection<Role> roles) {
+        return roles.stream()
+                .map(Role::getPrivileges)
+                .flatMap(Collection::stream)
+                .map(Privilege::getName)
+                .collect(Collectors.toList());
     }
 
-    private Collection<? extends GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        for (String privilege : privileges) {
-            authorities.add(new SimpleGrantedAuthority(privilege));
-        }
-        return authorities;
+    /**
+     * Function that parse all the privileges names to Granted Authority object for Spring management.
+     *
+     * @param privileges
+     * @return
+     */
+    private Collection<? extends GrantedAuthority> getGrantedAuthorities(Collection<String> privileges) {
+        return privileges.stream()
+                .map(s -> new SimpleGrantedAuthority(s))
+                .collect(Collectors.toList());
     }
 }
 
