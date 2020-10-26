ab801026ed50d5e70cb7761fe2186396f62064b9
Osvaldo Fernandez <osvaldof22@hotmail.com>
2020-10-26 14:45:02 -0400

    CORS bug fixed!

--- src/main/java/com/project/ccts/config/CorsConfig.java
+++ src/main/java/com/project/ccts/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
